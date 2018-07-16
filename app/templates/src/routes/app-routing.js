1c60ddd9040917542d24ffb55a216fec981cdd11
Viktor Lukashov <viktor@vaadin.com>
2018-07-16 13:08:53 +0300

    call the file with URL constants 'urls' (not 'pages')
    'pages' is easy to confuse with 'views'. It's not immediately clear that one is actual view web components for different in-app URLs and the other one is simple URL string constants.

--- app/templates/src/routes/app-routing.js
+++ app/templates/src/routes/app-routing.js
@@ -1,5 +1,5 @@
 import { Router } from '@vaadin/router';
-import { EMPLOYEE_LIST, NEW_EMPLOYEE } from './pages.js';
+import { EMPLOYEE_LIST, NEW_EMPLOYEE } from './urls.js';
 
 export function init(outlet) {
   const router = new Router(outlet);
